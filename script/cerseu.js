3055a440259126fd6891d9ced67a04a169de46c5
José David Alcalde <david20egt@gmail.com>
2025-06-12 16:42:37 -0500

    tratando de redireccionar a página de inicio el botón ENVIAR

--- script/cerseu.js
+++ script/cerseu.js
@@ -1,4 +1,3 @@
-
 // --- Efecto al hacer scroll: header transparente y con sombra ---
 window.addEventListener("scroll", function () {
   const header = document.querySelector("header");
@@ -9,11 +8,28 @@ window.addEventListener("scroll", function () {
   }
 });
 
+// document.getElementById("formulario").addEventListener("submit", function (e) {
+//   e.preventDefault();
 
-// --- Manejo del formulario de contacto ---
-document.getElementById("formulario").addEventListener("submit", function (e) {
-  e.preventDefault();
-  alert("Gracias por contactarte con CERSEU. Pronto te responderemos.");
-  this.reset();
-});
+//   const form = e.target;
+//   const formData = new FormData(form);
+
+//   fetch("https://script.google.com/macros/s/AKfycbw94V4JAy9qjtAGYueZj3vLSFgwA6iorXRsDhpOcInJ8IFMFTVqCjgKK7gIQ2IcWHp4/exec", {
+//     method: "POST",
+//     body: formData,
+//   })
+//   .then(response => response.text())
+//   .then(result => {
+//     // Opcional: mostrar mensaje de éxito
+//     alert("Formulario enviado correctamente ✅");
+
+//     // Redirigir después de enviar
+//     window.location.href = "https://economia.unmsm.edu.pe/FCE/cerseu.html";
+//   })
+//   .catch(error => {
+//     alert("Error al enviar el formulario ❌");
+//     console.error(error);
+//   });
+// });
+
 
